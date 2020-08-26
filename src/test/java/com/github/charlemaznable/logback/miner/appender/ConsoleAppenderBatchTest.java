01e0410d27c563bec02357099d6cf42d6bec024c
CharLemAznable <545541819@qq.com>
2020-08-26 17:37:02 +0800

    Add batch test.

---
+++ src/test/java/com/github/charlemaznable/logback/miner/appender/ConsoleAppenderBatchTest.java
@@ -0,0 +1,76 @@
+package com.github.charlemaznable.logback.miner.appender;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.n3r.diamond.client.impl.MockDiamondServer;
+
+import static java.lang.Runtime.getRuntime;
+import static java.lang.System.currentTimeMillis;
+import static java.time.Duration.ofMillis;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+public class ConsoleAppenderBatchTest {
+
+    public final int TIMES = 1000;
+
+    @SneakyThrows
+    public void batchRun(int times) {
+        for (int i = 0; i < times; ++i) {
+            await().pollDelay(ofMillis(10)).until(() -> true);
+        }
+    }
+
+    @SneakyThrows
+    public void batchRunLog(int times) {
+        for (int i = 0; i < times; ++i) {
+            await().pollDelay(ofMillis(10)).until(() -> true);
+            log.info("message");
+        }
+    }
+
+    @SneakyThrows
+    public void routineRun(int threadCount, Runnable target) {
+        val threads = new Thread[threadCount];
+        for (int i = 0; i < threadCount; i++) {
+            threads[i] = new Thread(target);
+            threads[i].start();
+        }
+
+        for (int i = 0; i < threadCount; i++) {
+            threads[i].join();
+        }
+    }
+
+    @Test
+    public void testConsoleAppenderBatch() {
+        ConsoleTarget.setUpMockConsole();
+        MockDiamondServer.setUpMockServer();
+        val future = MockDiamondServer.updateDiamond("Logback", "test",
+                "com.github.charlemaznable.logback.miner.appender.ConsoleAppenderBatchTest[ADDITIVITY]=no\n" +
+                        "com.github.charlemaznable.logback.miner.appender.ConsoleAppenderBatchTest[LEVEL]=INFO\n" +
+                        "com.github.charlemaznable.logback.miner.appender.ConsoleAppenderBatchTest[CONSOLE.CHARSET]=utf-8\n" +
+                        "com.github.charlemaznable.logback.miner.appender.ConsoleAppenderBatchTest[CONSOLE.PATTERN]=%5level %message%n\n" +
+                        "com.github.charlemaznable.logback.miner.appender.ConsoleAppenderBatchTest[CONSOLE.TARGET]=batch\n" +
+                        "com.github.charlemaznable.logback.miner.appender.ConsoleAppenderBatchTest[CONSOLE.IMMEDIATEFLUSH]=true\n");
+        await().forever().until(future::isDone);
+
+        val threadCount = getRuntime().availableProcessors() + 1;
+
+        val startTime = currentTimeMillis();
+        routineRun(threadCount, () -> batchRun(TIMES));
+        val batchRunTime = currentTimeMillis() - startTime;
+
+        val startLogTime = currentTimeMillis();
+        routineRun(threadCount, () -> batchRunLog(TIMES));
+        val batchRunLogTime = currentTimeMillis() - startLogTime;
+
+        assertTrue(batchRunLogTime < batchRunTime * 1.05); // 性能损耗小于5%
+
+        MockDiamondServer.tearDownMockServer();
+        ConsoleTarget.tearDownMockConsole();
+    }
+}
---
+++ src/test/java/com/github/charlemaznable/logback/miner/appender/DqlAppenderBatchTest.java
@@ -0,0 +1,119 @@
+package com.github.charlemaznable.logback.miner.appender;
+
+import com.github.bingoohuang.westid.WestId;
+import com.github.charlemaznable.logback.miner.annotation.LogbackBean;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.n3r.diamond.client.impl.MockDiamondServer;
+import org.n3r.eql.diamond.Dql;
+
+import java.util.Date;
+
+import static java.lang.Runtime.getRuntime;
+import static java.lang.System.currentTimeMillis;
+import static java.time.Duration.ofMillis;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+public class DqlAppenderBatchTest {
+
+    public final int TIMES = 1000;
+
+    private static final String DBBatch = "db_batch";
+    private static final String CREATE_TABLE_SIMPLE_LOG = "CREATE TABLE `SIMPLE_LOG` (" +
+            "  `LOG_ID` BIGINT NOT NULL," +
+            "  `LOG_CONTENT` TEXT," +
+            "  `LOG_DATE` DATETIME," +
+            "  PRIMARY KEY (`LOG_ID`)" +
+            ");\n";
+
+    @BeforeAll
+    public static void beforeAll() {
+        MockDiamondServer.setUpMockServer();
+        MockDiamondServer.setConfigInfo("EqlConfig", DBBatch,
+                "driver=org.h2.Driver\n" +
+                        "url=jdbc:h2:mem:db_batch;DB_CLOSE_DELAY=-1;MODE=MySQL;DATABASE_TO_LOWER=TRUE\n" +
+                        "user=\n" +
+                        "password=\n");
+
+        new Dql(DBBatch).execute("" +
+                CREATE_TABLE_SIMPLE_LOG);
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        MockDiamondServer.tearDownMockServer();
+    }
+
+    @SneakyThrows
+    public void batchRun(int times) {
+        for (int i = 0; i < times; ++i) {
+            await().pollDelay(ofMillis(10)).until(() -> true);
+        }
+    }
+
+    @SneakyThrows
+    public void batchRunLog(int times) {
+        for (int i = 0; i < times; ++i) {
+            await().pollDelay(ofMillis(10)).until(() -> true);
+            val simpleLog = new SimpleLog();
+            simpleLog.setLogId(Long.toString(WestId.next()));
+            simpleLog.setLogContent("simple log");
+            simpleLog.setLogDate(new Date());
+            log.info("simple log: {}", simpleLog);
+        }
+    }
+
+    @SneakyThrows
+    public void routineRun(int threadCount, Runnable target) {
+        val threads = new Thread[threadCount];
+        for (int i = 0; i < threadCount; i++) {
+            threads[i] = new Thread(target);
+            threads[i].start();
+        }
+
+        for (int i = 0; i < threadCount; i++) {
+            threads[i].join();
+        }
+    }
+
+    @Test
+    public void testDqlAppender() {
+        val future = MockDiamondServer.updateDiamond("Logback", "test",
+                "eql[console.level]=off\norg.n3r.eql[console.level]=off\n" +
+                        "com.github.charlemaznable.logback.miner.appender.DqlAppenderBatchTest[additivity]=no\n" +
+                        "com.github.charlemaznable.logback.miner.appender.DqlAppenderBatchTest[dql.level]=info\n" +
+                        "com.github.charlemaznable.logback.miner.appender.DqlAppenderBatchTest[dql.connection]=" + DBBatch + "\n" +
+                        "com.github.charlemaznable.logback.miner.appender.DqlAppenderBatchTest[console.level]=off");
+        await().forever().until(future::isDone);
+
+        val threadCount = getRuntime().availableProcessors() + 1;
+
+        val startTime = currentTimeMillis();
+        routineRun(threadCount, () -> batchRun(TIMES));
+        val batchRunTime = currentTimeMillis() - startTime;
+
+        val startLogTime = currentTimeMillis();
+        routineRun(threadCount, () -> batchRunLog(TIMES));
+        val batchRunLogTime = currentTimeMillis() - startLogTime;
+
+        assertTrue(batchRunLogTime < batchRunTime * 1.05); // 性能损耗小于5%
+    }
+
+    @Getter
+    @Setter
+    @LogbackBean
+    public static class SimpleLog {
+
+        private String logId;
+        private String logContent;
+        private Date logDate;
+    }
+}
